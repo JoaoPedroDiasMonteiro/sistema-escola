f751db5b1838ca07c227c616becdbaf53a28977f
João Pedro <joaopedro453@hotmail.com>
2020-09-21 05:41:03 -0300

    update web routes

--- routes/web.php
+++ routes/web.php
@@ -13,52 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'AppController@index');
-Route::get('/teachers', 'AppController@teacher');
-Route::get('/students', 'AppController@student');
-
-
-Route::get('/schedule/edit/{schedule}', 'ScheduleController@edit');
-Route::get('/schedule/delete/{schedule}', function (\Illuminate\Http\Request $request){
-    return view('app.scheduleDelete', ['schedule' => $request->schedule]);
+Route::get('/', function () {
+    return view('app.app');
 });
-Route::get('/schedules/new', 'ScheduleController@create');
 
+Route::resources([
+    'schedule' => ScheduleController::class,
+    'teacher' => TeacherController::class,
+    'student' => StudentController::class,
+]);
 
+Route::get('schedule/delete/{schedule}', function (\Illuminate\Http\Request $request) {
+    return view('app.schedule.delete', ['schedule' => $request->schedule]);
+});
 
-Route::get('/teacher/edit/{teacher}', 'TeacherController@edit');
-Route::get('/teacher/delete/{teacher}', function (\Illuminate\Http\Request $request){
+Route::get('teacher/delete/{teacher}', function (\Illuminate\Http\Request $request) {
     return view('app.teacher.delete', ['teacher' => $request->teacher]);
 });
-Route::get('/teachers/new', 'TeacherController@create');
 
-
-
-Route::get('/student/edit/{student}', 'StudentController@edit');
-Route::get('/student/delete/{student}', function (\Illuminate\Http\Request $request){
+Route::get('student/delete/{student}', function (\Illuminate\Http\Request $request) {
     return view('app.student.delete', ['student' => $request->student]);
 });
-Route::get('/students/new', 'StudentController@create');
-
-
-
-Route::get('/api/schedule', 'ScheduleController@index');
-Route::get('/api/schedule/all', 'ScheduleController@all');
-Route::get('/api/schedule/weekday/{weekday}', 'ScheduleController@weekday');
-Route::get('/api/schedule/weekday/{weekday}/all', 'ScheduleController@weekdayAll');
-Route::put('api/schedule/edit/{schedule}', 'ScheduleController@update');
-Route::delete('api/schedule/delete/{schedule}', 'ScheduleController@destroy');
-Route::post('api/schedule/new', 'ScheduleController@store');
-
-Route::get('/api/teacher', 'TeacherController@index');
-Route::get('/api/teacher/all', 'TeacherController@all');
-Route::put('api/teacher/edit/{teacher}', 'TeacherController@update');
-Route::delete('api/teacher/delete/{teacher}', 'TeacherController@destroy');
-Route::post('api/teacher/new', 'TeacherController@store');
-
-Route::get('/api/student', 'StudentController@index');
-Route::get('/api/student/all', 'StudentController@all');
-Route::put('api/student/edit/{student}', 'StudentController@update');
-Route::delete('api/student/delete/{student}', 'StudentController@destroy');
-Route::post('api/student/new', 'StudentController@store');
-
